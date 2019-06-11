ae6c6798ca0e00aeafbf4d1263d7352f2a9e0f5a
MarjanGit <mansar16@huntersoe.org>
2019-06-11 11:52:30 -0400

    student cards

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Grid from "./Grid";
 import AppRouter from "./navbar";
 import CampusListing from "./CampusListing";
+import Students from "./Students";
 class Home extends Component {
   constructor(props) {
     super(props);
@@ -13,6 +14,7 @@ class Home extends Component {
       <div>
         <Grid />
         <CampusListing />
+        <Students />
       </div>
     );
   }
