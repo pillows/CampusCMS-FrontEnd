caedc29b02c56902be8ced30859a146d6388e7d1
MarjanGit <mansar16@huntersoe.org>
2019-06-11 12:04:53 -0400

    student

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,26 @@ import logo from "./logo.svg";
 import "./App.css";
 import AppRouter from "./components/navbar";
 import Home from "./components/Home";
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
+import CampusListing from "./components/CampusListing";
+import Students from "./components/Students";
 
 function App() {
   return (
     <div className="App">
       <div>
-        <AppRouter />
-        <Home />
+        {/* <AppRouter /> */}
+        {/* <Home /> */}
+
+        <BrowserRouter>
+          <div className="App">
+            {/* <Route exact path="/" render={props => <PageLayout component={Login} {...props} />} /> */}
+            <Route exact path="/" component={Home} />
+            <Route path="/campus" component={CampusListing} />} />
+            <Route path="/students" render={Students} />
+          </div>
+        </BrowserRouter>
+
         <div className="" />
       </div>
     </div>
